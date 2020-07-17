cae286aba5ab90d56e3c45b1a3db926b926265d0
David Luis Wiegandt <wigandtd@hu-berlin.de>
2020-07-17 20:33:07 +0200

    rudimentary CLI working again

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,82 @@
 #!/usr/bin/env node
-import { Engine, Instance, Parser } from "@sigma-db/core";
-import { createServer, createConnection } from "net";
-import { pipeline } from "stream";
+import { createInterface } from "readline";
+import { Instance, Parser, Engine, Result, ResultType } from "@sigma-db/core";
+import { textSync } from "figlet";
+import * as chalk from "chalk";
+import * as Table from "cli-table3";
 import * as yargs from "yargs";
+import { version, dependencies } from "../package.json";
 
-yargs
-    .command(
-        "server",
-        "Serves a database instance loaded from a given log file and allows it to be modified using queries received on a given socket",
-        {
-            "log-file": { alias: "l", description: "The path to the log file of the database instance", type: "string" },
-            "socket": { alias: "s", description: "The UNIX domain socket to communicate on", type: "string", demandOption: true },
-        },
-        ({ "log-file": path, socket }) => {
-            const database = Instance.create({ path });
-            createServer().listen(socket)
-                .on("connection", socket => {
-                    const parser = Parser.create({ schema: database.schema });
-                    const engine = Engine.create({ instance: database });
-                    pipeline(
-                        socket,
-                        parser,
-                        engine,
-                        socket,
-                        err => err && console.error(err.message),
-                    );
-                })
-                .on("close", () => {
-                    database.close();
-                });
-        })
-    .command(
-        "client",
-        "Connects to the server on a given socket and allows to send queries against its served database instance",
-        {
-            "socket": { alias: "s", description: "The UNIX domain socket to communicate on", type: "string", demandOption: true },
-        },
-        ({ socket }) => {
-            const { stdin, stdout } = process;
-            const ipcSocket = createConnection({ path: socket, readable: true, writable: true });
-
-            ipcSocket
-                .on("connect", () => {
-                    pipeline(
-                        stdin,
-                        ipcSocket,
-                        stdout,
-                        err => err && stdout.write(err.message),
-                    );
-                });
-        }
-    )
+const { "log-file": path } = yargs
+    .option("log-file", { alias: "l", description: "The path to the log file of the database instance", type: "string" })
     .scriptName("sigma")
     .help()
     .argv;
+
+const initStdin = () => {
+// process.stdin.setRawMode(true);
+// process.stdin.on("keypress", (_str, key) => {
+//     if (key.shift && key.name === "enter") {
+//         process.stdout.write("\n  ");
+//     }
+// });
+}
+
+const printBanner = () => {
+    console.log(chalk.cyan(textSync("sigmaDB", { horizontalLayout: "fitted" })));
+    console.log();
+    console.log(chalk.cyan(`This is sigmaDB CLI v${version} using sigmaDB v${dependencies["@sigma-db/core"].slice(1)}`));
+    console.log();
+}
+
+const format = (result: Result) => {
+    switch (result.type) {
+        case ResultType.SUCCESS:
+            return chalk.green("Done.");
+        case ResultType.ERROR:
+            return chalk.red(result.message);
+        case ResultType.RELATION:
+            const table = new Table({
+                head: result.relation.schema.map(attr => attr.name),
+                style: { head: ["white"] }
+            });
+            for (const tuple of result.relation.tuples()) {
+                table.push(Object.values(tuple).map(value => {
+                    switch (typeof value) {
+                        case "string":
+                            return chalk.yellow(`"${value}"`);
+                        case "number":
+                            return chalk.magenta(value);
+                        case "boolean":
+                            return value ? chalk.green("true") : chalk.red("false");
+                    }
+                }));
+            }
+            return `${result.relation.name ?? ""} (${result.relation.size} ${result.relation.size === 1 ? "tuple" : "tuples"}):\n${table.toString()}`;
+    }
+}
+
+const main = async () => {
+    initStdin();
+    printBanner();
+
+    const database = Instance.create({ path });
+    const parser = Parser.create();
+    const engine = Engine.create();
+    const repl = createInterface(process.stdin);
+
+    process.stdout.write("> ");
+    repl.prompt();
+
+    for await (const line of repl) {
+        for (const statement of parser.parse(line)) {
+            const result = engine.evaluate(statement, database);
+            const output = format(result);
+            console.log(output);
+        }
+        process.stdout.write("> ");
+        repl.prompt();
+    }
+}
+
+main();
